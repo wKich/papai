94181a84d2a7e250736dbdb06fb09ff3d1e64c04
Dmitriy Lazarev <w@kich.dev>
2026-04-06 11:32:51 +0500

    refactor: move client source files to client/debug/

--- src/debug/dashboard/dashboard-state.ts
+++
@@ -1,9 +0,0 @@
-/// <reference lib="dom" />
-export * from './state.js'
-export * from './handlers.js'
-export * from './logs.js'
-export * from './sse.js'
-export * from './search.js'
-
-// Initialize the dashboard
-import './init.js'
